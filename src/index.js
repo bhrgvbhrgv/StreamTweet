abb86ede78d21199ff9a178a844420772cd79a4e
bhrgvbhrgv <bhargavdas21th@gmail.com>
2025-07-31 16:37:56 +0530

    cookie-parser and cors installed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ dotenv.config({
     path: "./.env"
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})
+.catch((error) => {
+    console.error("mongo db connection failed:", error);});
+    
 
 import express from "express";
 const app = express();
